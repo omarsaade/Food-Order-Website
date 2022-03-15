cf1d2763a240382677765b006eafc8593c89169d
omar saade <omarsaadeh11@gmail.com>
2022-03-15 11:26:14 +0200

    mysql

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -13,6 +13,17 @@ The print_r() function prints the information about a variable in a more human-r
             unset($_SESSION['add']);
         }
 
+
+        if (isset($_SESSION['remove'])) {
+            echo $_SESSION['remove'];
+            unset($_SESSION['remove']);
+
+            if (isset($_SESSION['delete'])) {
+                echo $_SESSION['delete'];
+                unset($_SESSION['delete']);
+            }
+        }
+
         ?>
 
         <br><br>
@@ -95,7 +106,7 @@ The print_r() function prints the information about a variable in a more human-r
                         <td><?php echo $active; ?></td>
                         <td>
                             <a href="#" class="btn-secondary">Update Category</a>
-                            <a href="#" class="btn-danger">Delete Category</a>
+                            <a href="<?php echo SITEURL; ?>admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Category</a>
                         </td>
                     </tr>
 
